4ef1aa9d24d27e888f1395f2403ba37f3738e691
abolfazl <abolfazlkhazaei999@gmail.com>
2023-10-27 22:24:19 +0330

    start

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/modules/Navbar/Navbar";
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />;
+    </>
+  );
+}
